826d9cd689b068781283611ae5e49a1eadeb1ce7
CubitSpeed <67028273+CubitSpeed@users.noreply.github.com>
2021-08-08 11:27:32 -0700

    Update SwiftAudioStreamerPlugin.swift

--- packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
+++ packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
@@ -66,9 +66,6 @@ public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHan
     func startRecording() {
         engine = AVAudioEngine()
       
-        try! AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, options: .mixWithOthers)
-        try! AVAudioSession.sharedInstance().setActive(true)
-      
         let input = engine.inputNode
         let bus = 0
 
